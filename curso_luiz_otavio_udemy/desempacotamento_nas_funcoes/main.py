55af71969476ad1f7b0ba80279666d92a87bb8bc
kirestein <erik.proenca2011@gmail.com>
2023-03-31 13:44:33 -0300

    created curso_luiz_otavio_udemy folder

---
+++ curso_luiz_otavio_udemy/desempacotamento_nas_funcoes/main.py
@@ -0,0 +1,9 @@
+nomes = ['Erik', 'Roberto', 'Cláudio', 'Letíca', 'Cristhiane', 'Joelma']
+pessoa = {
+    'nome': 'Cristhiane',
+    'idade': '38',
+    'cidade': 'São Paulo'
+}
+
+
+print(*nomes, sep=', ')
