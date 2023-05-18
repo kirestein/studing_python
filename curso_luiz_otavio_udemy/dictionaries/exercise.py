3bea6890ee02fed49e9aebb7a29556aa09b918bd
kirestein <erik.proenca2011@gmail.com>
2023-05-17 22:00:06 -0300

    resolved exercise

--- curso_luiz_otavio_udemy/dictionaries/exercise.py
+++ curso_luiz_otavio_udemy/dictionaries/exercise.py
@@ -19,3 +19,35 @@ perguntas = [
         'Resposta': '5',
     },
 ]
+
+acertos = 0
+erros = 0
+for pergunta in perguntas:
+    print(f"{pergunta['Pergunta']}")
+    print('')
+    opcao_selecionada = 0
+    for num, opcao in enumerate(pergunta['Opções']):
+        print(f'{num}) {opcao}')
+        if num == len(pergunta['Opções']) - 1:
+            resposta = input('Selecione uma opção: ')
+            while True:
+                if resposta.isdigit():
+                    opcao_selecionada = int(resposta)
+                    break
+                else:
+                    resposta = input('Selecione um opção válida: ')
+    print('')
+    for num, opcao in enumerate(pergunta['Opções']):
+        if opcao_selecionada == num:
+            opcao_selecionada = opcao
+    print(opcao_selecionada)
+    # resposta = input('Digite a resposta: ')
+    if opcao_selecionada == pergunta['Resposta']:
+        print('Você acertou! ✅')
+        acertos += 1
+    else:
+        print('Você errou! ❌')
+        erros += 1
+    print('-' * 50)
+print(f'Você acertou {acertos} questões!')
+print(f'Você errou {erros} questões!')
