15249ccb48cf7d8b1c80aa28aa12612d4c769d46
kirestein <erik.proenca2011@gmail.com>
2023-06-30 19:38:50 -0300

    [PySide6] Moved all files to aulas's folder In this folder'll be store all the classes

--- py_side_06/aulas/aula_06_QLine_QLabel.py
+++ py_side_06/aulas/aula_06_QLine_QLabel.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QLineEdit, QFrame, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QMainWindow, QLineEdit, QFrame, QVBoxLayout, QLabel
 from PySide6.QtCore import QSize, Qt
 
 class MainWindow(QMainWindow):
@@ -12,9 +12,11 @@ class MainWindow(QMainWindow):
         #? Criamos o LineEdit atribuindo ele a uma variável
         self.input = QLineEdit()
         self.input.setPlaceholderText('Digite algo')
+        self.lbl = QLabel('Digite algo')
         
         #? Criamos um layout e adicionamos o LineEdit a ele
         layout = QVBoxLayout()
+        layout.addWidget(self.lbl)
         layout.addWidget(self.input)
         
         #? Criamos a janela e adicionamos o layout a ele
@@ -22,6 +24,12 @@ class MainWindow(QMainWindow):
         container.setLayout(layout)
         
         self.setCentralWidget(container)
+        
+        self.input.textChanged.connect(self.text_changed)
+        
+    #? Função para alterar o texto do QLabel
+    def text_changed(self):
+        self.lbl.setText(self.input.text())
         
         
 app = QApplication(sys.argv)
