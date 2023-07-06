7c1d3234f41156efd13e61dccf1162ae33fe9f68
kirestein <erik.proenca2011@gmail.com>
2023-07-06 00:01:30 -0300

    [hashtag] Studing list comprehension

---
+++ hashtag/12_list_comprehensions/Gabarito_16.03.01 Exercícios List Comprehension.py
@@ -0,0 +1,39 @@
+{
+ "cells": [
+  {
+   "attachments": {},
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# Exercícios\n",
+    "\n",
+    "## 1. Filtrar lista de clientes inadimplentes\n",
+    "\n",
+    "- O objetivo é identificar quem são os clientes inadimplentes e enviar essa lista de clientes para o setor de cobrança poder fazer a cobrança dos clientes.\n",
+    "- Queremos então criar uma lista com os clientes inadimplentes (apenas o CPF e o valor que eles estão devendo)\n",
+    "- A inadimplência nessa empresa é calculada da seguinte forma:\n",
+    "    1. Se o cliente tiver devendo há mais de 20 dias, ele é considerado inadimplente.\n",
+    "    2. As informações vêm no formato (cpf, valor_devido, qtde de dias)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {
+    "vscode": {
+     "languageId": "plaintext"
+    }
+   },
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
